f13f0e58be994eb7648646ae08b90b525aec5be0
Nzix <iminezn5656@outlook.com>
2018-10-10 00:12:51 +0800

    fix cdn hit miss by redirect

--- README.md
+++ README.md
@@ -1,26 +1,3 @@
-# NetEaseMusicWorld [![Chrome Web Store](https://img.shields.io/chrome-web-store/v/kmaboigdbjjclfomcbpafdclllbinmam.svg)](https://chrome.google.com/webstore/detail/neteasemusicworld/kmaboigdbjjclfomcbpafdclllbinmam)
+# NetEaseMusicWorld
 
-安装插件。
-
-并且在`hosts`文件里写入如下内容：
-
-```
-115.127.226.166  p1.music.126.net
-115.127.226.166  p3.music.126.net
-115.127.226.166  p4.music.126.net
-115.127.227.115  m10.music.126.net
-```
-
-即可在海外听网易云音乐～
-
-## 关于优化IP地址
-
-默认给出的是日本的IP地址，p开头的是图片CDN服务器，m开头的是音乐资源CDN服务器。可以通过
-```
-ping ws.acgvideo.com
-```
-来寻找最优的音乐资源CDN服务器。通过
-```
-ping cdnetworks.com
-```
-来寻找最优的图片资源CDN服务器。
+安装插件, 即可在海外用网易云音乐网页版听歌 (点击图标开关) ~
--- chrome/background.js
+++ chrome/background.js
@@ -1,12 +1,35 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'loading') {
-    chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
-      const header = {
-        name: 'X-Real-IP',
-        value: '211.161.244.70'
-      }
-      details.requestHeaders.push(header)
-      return {requestHeaders: details.requestHeaders}
-    }, {urls: ['*://music.163.com/*']}, ['blocking', 'requestHeaders'])
+let toggle
+
+// hook request headers
+chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
+  const header = {
+    name: 'X-Real-IP',
+    value: '211.161.244.70'
   }
+  if (toggle) details.requestHeaders.push(header)
+  return { requestHeaders: details.requestHeaders }
+}, { urls: ['*://music.163.com/*'] }, ['blocking', 'requestHeaders'])
+
+// redirect cdn access
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+  const redirectUrl = details.url.replace(
+    /(m\d+?)(?!c)\.music\.126\.net/, '$1c.music.126.net'
+  )
+  if (toggle) return { redirectUrl: redirectUrl }
+}, { urls: ['*://*.music.126.net/*'] }, ['blocking'])
+
+// toggle button
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.storage.local.get('toggle', function (data) {
+    chrome.storage.local.set({ toggle: !data.toggle })
+    toggle = !data.toggle
+    chrome.browserAction.setBadgeText({ text: ((toggle) ? 'ON' : 'OFF') })
+  })
+})
+
+// initialize
+chrome.storage.local.get('toggle', function (data) {
+  toggle = (typeof(data.toggle) === "undefined") ? true : data.toggle
+  chrome.storage.local.set({ toggle: toggle })
+  chrome.browserAction.setBadgeText({ text: ((toggle) ? 'ON' : 'OFF') })
 })
